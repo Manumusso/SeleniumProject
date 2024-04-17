77de0626bbc2b2cf6dad6569110d5416c316e909
manumusso05@gmail.com <manumusso05@gmail.com>
2024-04-17 09:03:29 -0300

    ConfigInitial Selenium

---
+++ HelloWorldProject/src/main/java/HelloWorldApp.java
@@ -0,0 +1,12 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HelloWorldApp {
+    public static void main(String[] args){
+        //System.out.print("Hello world");
+        System.setProperty("webdriver.chrome.driver",
+                "drivers/chromedriver.exe");
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("http://google.com");
+
+    }
+}
---
+++ HelloWorldProject/src/test/java/HelloWorldTest.java
@@ -0,0 +1,8 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+public class HelloWorldTest {
+    @Test
+    public void HelloTest(){
+        assertEquals("Hello", "Hello");
+    }
+}
